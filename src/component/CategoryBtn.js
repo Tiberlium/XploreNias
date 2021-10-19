12f97062e1edc0050471a92c38506c7fc5efb1bd
Tiberlium <lonew047@gmail.com>
2021-10-19 17:45:45 +0700

    Perubahan data

--- src/component/CategoryBtn.js
+++ src/component/CategoryBtn.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, TouchableOpacity, StyleSheet} from 'react-native';
+import {View, TouchableOpacity, StyleSheet,Text} from 'react-native';
 import {Card} from 'react-native-paper';
 import Icon from 'react-native-vector-icons/Feather';
 
