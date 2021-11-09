5d920a3e8c5409fb6754858a37e274e5e34b186e
Tiberlium <lonew047@gmail.com>
2021-11-10 02:21:41 +0700

    perbaikan

--- src/screen/Register/index.js
+++ src/screen/Register/index.js
@@ -56,7 +56,6 @@ export default function Register({navigation}) {
       />
       <Headline style={styles.txtTitle}>Daftar</Headline>
 
-
       <Text style={styles.label}>Email</Text>
       <TextInput
         placeholder="Masukkan Email di sini"
@@ -65,22 +64,15 @@ export default function Register({navigation}) {
         style={styles.txtInput}
       />
 
-      <View style={{display:'flex',flexDirection:'row',justifyContent:'space-around'}}>
-        <Text style={styles.label1}>Password</Text>
-        <TouchableOpacity onPress={() => navigation.navigate('Passrecovery')}>
-          <Text style={styles.forget}>Lupa Password?</Text>
-        </TouchableOpacity>
-      </View>
-
+      <Text style={styles.label}>Password</Text>
 
       <TextInput
         placeholder="Masukkan Password di sini"
         value={Password}
         onChangeText={setPassword}
         style={styles.txtInput}
-        secureTextEntry = {true}
+        secureTextEntry={true}
       />
-
 
       <Button mode="contained" style={styles.btn} onPress={submit}>
         Daftar
@@ -98,10 +90,9 @@ export default function Register({navigation}) {
         <Logobtn source={require('../../Asset/Image/facebook.png')} />
       </View>
 
-
       <View style={styles.wrapUnlog}>
         <Text style={styles.txtUnlog}>Sudah terdaftar?</Text>
-        <TouchableOpacity onPress={()=>navigation.navigate('Login')}>
+        <TouchableOpacity onPress={() => navigation.navigate('Login')}>
           <Text style={styles.txtUnlogBtn}>Masuk</Text>
         </TouchableOpacity>
       </View>
@@ -110,7 +101,12 @@ export default function Register({navigation}) {
 }
 
 const styles = StyleSheet.create({
-  icon: {marginHorizontal: wp(33), width: wp(30), height: hp(16), marginVertical: hp(5)},
+  icon: {
+    marginHorizontal: wp(33),
+    width: wp(30),
+    height: hp(16),
+    marginVertical: hp(4),
+  },
   txtTitle: {
     marginHorizontal: wp(37),
     padding: 10,
@@ -121,14 +117,9 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     color: 'black',
     marginLeft: wp(12),
+    marginVertical:hp(1),
   },
-  label1: {
-    fontSize: hp(2.5),
-    fontWeight: 'bold',
-    color: 'black',
-    marginTop:hp(2),
-  },
-  forget:{color:'red',fontSize:hp(2),marginTop:hp(2.5)},
+  forget: {color: 'red', fontSize: hp(2), marginTop: hp(2.5)},
   txtInput: {
     borderWidth: 0.4,
     width: wp('80'),
@@ -161,7 +152,12 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     marginVertical: hp(0.5),
   },
-  txtAtau:{fontSize:hp('2'),marginHorizontal:wp('32'),marginTop:hp(2)},
+  txtAtau: {fontSize: hp('2'), marginHorizontal: wp('32'), marginTop: hp(2)},
   txtUnlog: {fontSize: 15},
-  txtUnlogBtn: {fontSize:hp('2'),fontWeight:'bold',color:'black',marginLeft:wp(2)},
+  txtUnlogBtn: {
+    fontSize: hp('2'),
+    fontWeight: 'bold',
+    color: 'black',
+    marginLeft: wp(2),
+  },
 });
